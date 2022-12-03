32db5f14f09131ac0bc986c217f47dd4017fbb19
iaqkrie <iaqkrie@yandex.ru>
2022-12-03 18:31:35 +0300

    lab4-add

---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab4 {
+	public partial class Form1 : Form {
+		public Form1 () {
+			InitializeComponent();
+		}
+
+		int n;
+		bool p;
+		private void button1_Click (object sender, EventArgs e) {
+			if (int.TryParse(textBox1.Text, out int num)) n = num;
+			else {
+				textBox2.Text = "Неверный формат";
+				return;
+			}
+
+			if (n <= 1) {
+				textBox2.Text = "Неверный формат";
+				return;
+			}
+
+			p = true;
+			for (int i = 2; i <= n / 2; i++) {
+				if (n % i == 0)
+					p = false;
+			}
+
+			textBox2.Text = p.ToString();
+		}
+	}
+}
---
+++ Lab4/Lab4/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab4 {
+	public partial class Form2 : Form {
+		public Form2 () {
+			InitializeComponent();
+		}
+
+		int x, n;
+		private void button1_Click (object sender, EventArgs e) {
+			if (int.TryParse(textBox1.Text, out int xv) && xv >= 0)
+				x = xv;
+			else {
+				textBox3.Text = "Неверный формат";
+				return;
+			}
+
+			if (int.TryParse(textBox2.Text, out int nv) && nv >= 0)
+				n = nv;
+			else {
+				textBox3.Text = "Неверный формат";
+				return;
+			}
+
+			double sum = 0;
+			for (int i = 1; i < 10; i++) {
+				for (int j = 1; j < n; j++) {
+					sum += Math.Sin(Math.Pow(x, 5) + Math.Pow(j, 4)) + 5;
+				}
+			}
+
+			textBox3.Text = sum.ToString();
+		}
+	}
+}
