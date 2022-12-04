6ea39275e0037bcb0bfcce07a2d0bb6210ee0f4a
iaqkrie <iaqkrie@yandex.ru>
2022-12-04 23:12:59 +0300

    lab5-add

---
+++ Lab5/Lab5/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lab5 {
+	public partial class Form1 : Form {
+		public Form1 () {
+			InitializeComponent();
+		}
+
+		int[] arr = new int[15];
+		int[] newArr = new int[15];
+		int am;
+
+		private void button1_Click (object sender, EventArgs e) {
+			Random rnd = new Random();
+			textBox1.Text = "";
+			for (int i = 0; i < 15; i++)
+				textBox1.Text += $"{rnd.Next(20)} ";
+		}
+
+		private void button2_Click (object sender, EventArgs e) {
+			if (textBox1.Text.Split().Length < 15) {
+				textBox2.Text = "Неверный формат";
+				return;
+			}
+			for (int i = 0; i < 15; i++) {
+				if (int.TryParse(textBox1.Text.Split()[i], out int n))
+					arr[i] = n;
+				else {
+					textBox2.Text = "Неверный формат";
+					return;
+				}
+			}
+			am = arr.Sum() / arr.Length;
+			int min = 101;
+			foreach (int n in arr)
+				if (n < min)
+					min = n;
+			for (int i = 0; i < 15; i++) {
+				if (arr[i] == min)
+					newArr[i] = am;
+				else
+					newArr[i] = arr[i];
+			}
+
+			textBox2.Text = "";
+			foreach (int n in newArr)
+				textBox2.Text += $"{n} ";
+		}
+	}
+}
---
+++ Lab5/Lab5/Form2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lab5 {
+	public partial class Form2 : Form {
+		public Form2 () {
+			InitializeComponent();
+		}
+
+		int n;
+		int[] a, b;
+		List<int> indexes;
+
+		private void button1_Click (object sender, EventArgs e) {
+			if (int.TryParse(textBox1.Text, out int num))
+				n = num;
+			else {
+				textBox2.Text = "Неверный формат данных";
+				return;
+			}
+
+			textBox2.Text = "";
+			Random rnd = new Random();
+			for (int i = 0; i < n; i++) {
+				if (i < n - 1)
+					textBox2.Text += $"{rnd.Next(100)} ";
+				else
+					textBox2.Text += $"{rnd.Next(100)}";
+			}
+		}
+
+		private void button2_Click (object sender, EventArgs e) {
+			if (int.TryParse(textBox1.Text, out int num))
+				n = num;
+			else {
+				textBox3.Text = "Неверный формат данных";
+				return;
+			}
+
+			if (textBox2.Text.Split().Length == n) {
+				a = new int[n];
+				for (int i = 0; i < n; i++) {
+					if (int.TryParse(textBox2.Text.Split()[i], out int anum))
+						a[i] = anum;
+					else {
+						textBox3.Text = "Неверный формат данных";
+						return;
+					}
+				}
+			} else {
+				textBox3.Text = "Неверный формат данных";
+				return;
+			}
+
+			indexes = new List<int>();
+			for (int i = 0; i < n; i++) {
+				int[] tmp = new int[n - i - 1];
+				for (int j = 0; j < tmp.Length; j++)
+					tmp[j] = a[i + j + 1];
+
+				if (tmp.Contains(a[i])) {
+					if (indexes.Contains(i))
+						continue;
+					indexes.Add(i);
+					for (int j = 0; j < tmp.Length; j++) {
+						if (tmp[j] == a[i])
+							indexes.Add(i + j + 1);
+					}
+				}
+			}
+
+			b = new int[a.Length - indexes.Count];
+			int bi = 0;
+			for (int i = 0; i < a.Length; i++) {
+				if (indexes.Contains(i))
+					continue;
+				else {
+					b[bi] = a[i];
+					bi++;
+				}
+			}
+
+			textBox3.Text = "";
+			foreach (int bn in b)
+				textBox3.Text += $"{bn} ";
+		}
+	}
+}
